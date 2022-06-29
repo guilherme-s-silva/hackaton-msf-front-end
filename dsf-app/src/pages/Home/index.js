8ed3e7755186af449147ade3e698a190aecfcb29
Silva Filho <tarusilva@gmail.com>
2022-06-29 15:48:49 -0300

    Criação do card-resumo dos tweets.

--- dsf-app/src/pages/Home/index.js
+++ dsf-app/src/pages/Home/index.js
@@ -1,10 +1,13 @@
+import CardBriefTweet from '../../components/CardBriefTweet';
 import './styles.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container-cards-brief" ></div>
+            <CardBriefTweet title={5000.15678678} userTwitter={"@userTeste"} />
+        </div>
+    );
 }
 
 export default App;
