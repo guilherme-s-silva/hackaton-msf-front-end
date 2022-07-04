8a662c77117efac3dbb8c4fb2c164e01d8f51ead
Silva Filho <tarusilva@gmail.com>
2022-07-03 21:43:56 -0300

    Remoção de useState sem uso.

--- dsf-app/src/pages/Home/index.js
+++ dsf-app/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { instanceAxios } from "../../services/axios";
 import { useGlobal } from "../../hooks/useGlobal";
